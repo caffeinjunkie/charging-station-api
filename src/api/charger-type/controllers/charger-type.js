e2feccdf534b689ab9f9ebe0e66326b32d5f944a
caffeinjunkie <dev.satrio.ap@gmail.com>
2022-06-12 18:38:41 +0700

    initial commit

---
+++ src/api/charger-type/controllers/charger-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  charger-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::charger-type.charger-type');
